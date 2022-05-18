eefb7f617900e151f3bac7d28b9548f7a26aa398
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-05-18 20:48:21 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -15,6 +15,8 @@ TEST_CASE("reports average, minimum and maximum") {
 
 TEST_CASE("average is NaN for empty array") {
   auto computedStats = Statistics::ComputeStatistics({});
+    
+  std::cout << computedStats.average << std::endl;  
   REQUIRE(std::isnan(computedStats.average) == true);
   REQUIRE(std::isnan(computedStats.max) == true);
   REQUIRE(std::isnan(computedStats.min) == true);
