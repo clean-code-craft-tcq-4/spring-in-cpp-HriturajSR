cb25daf204e9a51387d451e21a021c6e926743e1
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-05-18 20:06:46 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::StatMember Statistics::ComputeStatistics(const std::vector<double> &Input) {
+  Statistics::StatMember stats;
+    
+  if (!numbers.size()) {
+    stats.average = std::numeric_limits<double>::quiet_NaN();
+    stats.max = std::numeric_limits<double>::quiet_NaN();
+    stats.min = std::numeric_limits<double>::quiet_NaN();
+
+    return stats;
+  }
+  stats.min = *std::min_element(number.begin(), number.end());
+  stats.max = *std::max_element(number.begin(), number.end());
+  stats.average =
+      std::accumulate(number.begin(), number.end(), 0.000) / number.size();
+
+  return stats;
+
 }
