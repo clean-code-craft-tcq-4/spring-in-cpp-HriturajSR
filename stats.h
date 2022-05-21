756c9b1bc1b149072543125c99876efdd7d31204
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-05-21 20:31:09 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -14,4 +14,49 @@ public:
 };
     StatMember ComputeStatistics(const std::vector<double> &Input);
 }
+class IAlerter
+{
+public:
+	virtual void updateState() = 0;
+};
 
+class EmailAlert : public IAlerter
+{
+public:
+	bool emailSent;
+	EmailAlert()
+	{
+		emailSent = false;
+	}
+
+	void updateState() override
+	{
+		emailSent = true;
+	}
+};
+class LEDAlert : public IAlerter
+{
+public:
+	bool ledGlows;
+	LEDAlert()
+	{
+		ledGlows = false;
+	}
+	void updateState() override
+	{
+		ledGlows = true;
+	}
+};
+
+class StatsAlerter
+{
+	int maxT;
+	std::vector<IAlerter*> Alerts;
+public:
+	StatsAlerter(int maxThreshold, std::vector<IAlerter*> Alerters)
+	{
+		maxT = maxThreshold;
+		Alerts = Alerters;
+	}
+	void checkAndAlert(const std::vector<float>& data);
+};
