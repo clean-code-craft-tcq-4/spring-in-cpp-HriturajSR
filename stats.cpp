dc620d5908119faea3879390d116597f584d0446
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-05-21 20:57:04 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,7 +3,7 @@
 Statistics::StatMember Statistics::ComputeStatistics(const std::vector<double> &Input) {
   Statistics::StatMember stats;
     
-  if (!Input.size()) {
+ if (!Input.size()) {
     stats.average = std::numeric_limits<double>::quiet_NaN();
     stats.max = std::numeric_limits<double>::quiet_NaN();
     stats.min = std::numeric_limits<double>::quiet_NaN();
@@ -13,21 +13,21 @@ Statistics::StatMember Statistics::ComputeStatistics(const std::vector<double> &
   stats.min = *std::min_element(Input.begin(), Input.end());
   stats.max = *std::max_element(Input.begin(), Input.end());
   double avg = 0;
-	for (double input : Input)
-		{
-			avg = avg + input;
-		}
-		stats.average = avg / Input.size();
+  for (double input : Input)
+  {
+    avg = avg + input;
+   }
+  stats.average = avg / Input.size();
   return stats;
 
 }
 void StatsAlerter::checkAndAlert(const std::vector<float>& Input)
 {
-	float max = Input[0];
-	max = *max_element(Input.begin(), Input.end());
-	if (max > maxT)
-	{
-		Alerts[0]->updateState();
-		Alerts[1]->updateState();
-	}
+  // float max = Input[0];
+  // max = *max_element(Input.begin(), Input.end());
+   if (*max_element(Input.begin(), Input.end()) > maxT)
+   {
+    Alerts[0]->updateState();
+    Alerts[1]->updateState();
+   }
 }
